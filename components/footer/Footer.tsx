31805f93f5ed4e0e75e16a14d9c3adb0a9e121b6
Anna Li <anna.li@bekk.no>
2025-01-30 12:53:15 +0100

    make footer sticky

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="row-start-2 row-end-2 bg-gray-800 text-white">
+    <footer className="sticky bottom-0 left-0 right-0 row-start-2 row-end-2 bg-gray-800 text-white">
       <p>Footer</p>
     </footer>
   );

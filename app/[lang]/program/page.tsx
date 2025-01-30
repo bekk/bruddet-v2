36774c2dbe02f25f3927dc283168201353a1c07d
Anna Li <anna.li@bekk.no>
2025-01-30 12:54:39 +0100

    format fixes

--- app/[lang]/program/page.tsx
+++ app/[lang]/program/page.tsx
@@ -1,7 +1,8 @@
-
-export default async function Page({params}: {params: Promise<{lang: string}>}) {
-    const lang = (await params).lang;
-    return (
-        <h1>{lang} PROGGIS</h1>
-    );
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
+  const lang = (await params).lang;
+  return <h1>{lang} PROGGIS</h1>;
 }

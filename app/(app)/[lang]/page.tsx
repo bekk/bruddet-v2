63f28105dbe8e204cfbf8166cc872852ae7dec18
Anna Li <anna.li@bekk.no>
2025-02-03 10:53:39 +0100

    made frontpage title responsive

--- app/(app)/[lang]/page.tsx
+++ app/(app)/[lang]/page.tsx
@@ -4,22 +4,27 @@ import { FRONTPAGE_QUERY } from "@/sanity/lib/queries/frontPage";
 import { FRONTPAGE_QUERYResult } from "@/sanity/types/types";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-export default async function Page({ params }: { params: Promise<{ lang: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
   const lang = (await params).lang;
-  const frontPage: FRONTPAGE_QUERYResult = await client.fetch(FRONTPAGE_QUERY, { lang });
+  const frontPage: FRONTPAGE_QUERYResult = await client.fetch(FRONTPAGE_QUERY, {
+    lang,
+  });
   const minHeight = "calc(100vh-theme('spacing.12'))";
 
   return (
     <div
       className={`flex justify-center items-center min-h-front-page-height bg-cover bg-center`}
-      style={{ 
+      style={{
         minHeight: minHeight,
-        backgroundImage: `url(${urlFor(frontPage?.customImage?.asset?._ref as SanityImageSource).url()})` 
+        backgroundImage: `url(${urlFor(frontPage?.customImage?.asset?._ref as SanityImageSource).url()})`,
       }}
-
-      aria-label={frontPage?.customImage?.alt || ''}
+      aria-label={frontPage?.customImage?.alt || ""}
     >
-      <h1 className="text-[clamp(2rem,5vw,4rem)]">{frontPage?.title}</h1>
+      <h1>{frontPage?.title}</h1>
     </div>
   );
 }

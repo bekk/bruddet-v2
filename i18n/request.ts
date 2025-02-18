0d05416cb7a7c9edde13e1a47f9319a59e1488c8
Anna Li <anna.li@bekk.no>
2025-02-18 22:05:19 +0100

    typesafe

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,13 +1,17 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
+enum Locale {
+  nb = "nb",
+  en = "en",
+}
+
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
   // Ensure that a valid locale is used
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (!locale || !routing.locales.includes(locale as Locale)) {
     locale = routing.defaultLocale;
   }
 

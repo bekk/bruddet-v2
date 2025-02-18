4b0535170532fd80ab86277c23fc2069f0a35018
Anna Li <anna.li@bekk.no>
2025-02-18 15:40:27 +0100

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ['cdn.sanity.io'],
-    }
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);

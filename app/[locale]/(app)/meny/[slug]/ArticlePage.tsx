b3806beeca2db60be0f2f23ac4f09a0d3e246432
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-02-24 16:24:53 +0100

    WIP

--- app/[locale]/(app)/meny/[slug]/ArticlePage.tsx
+++ app/[locale]/(app)/meny/[slug]/ArticlePage.tsx
@@ -16,12 +16,14 @@ export const ArticlePage = async ({ data, language }: ArticlePageProps) => {
 
   return (
     <div className="flex flex-col w-full items-center gap-10">
-      <h1 className="text-3xl lg:text-6xl text-4xl font-normal text-center mt-40">
-        {title}
-      </h1>
-      <h2 className="text-xl lg:text-3xl md:text-left text-center max-w-[1000px] font-normal lg:leading-[48px]">
-        {ingress}
-      </h2>
+      <div className="flex flex-col gap-10">
+        <h1 className="text-3xl lg:text-6xl text-4xl font-normal text-center mt-40">
+          {title}
+        </h1>
+        <h2 className="text-xl lg:text-3xl text-left lg:text-center font-normal lg:leading-[48px]">
+          {ingress}
+        </h2>
+      </div>
       <TagButtons tagTexts={tagTexts} />
       {event && (
         <Link href={`/${language}/program/${event.slug?.current}`}>

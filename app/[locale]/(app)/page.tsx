288cfdcc0d5876bfc64aedf9a39055f22997c0e1
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-02-27 17:25:53 +0100

    trigger news letter open with hexagon buy button

--- app/[locale]/(app)/page.tsx
+++ app/[locale]/(app)/page.tsx
@@ -1,4 +1,4 @@
-import { Newsletter } from "@/components/newsletter/Newsletter";
+import HexagonBuyButton from "@/components/HexagonBuyButton";
 import { urlFor } from "@/sanity/lib/image";
 import { sanityFetch } from "@/sanity/lib/live";
 import { FRONTPAGE_QUERY } from "@/sanity/lib/queries/frontPage";
@@ -25,6 +25,10 @@ export default async function Page({
             }}
             aria-label={data?.image?.alt || ""}
         >
+            <div className="absolute lg:right-24 lg:top-24 right-6 top-16">
+                <HexagonBuyButton text="hei" />
+            </div>
+
             <h1 className="text-primary-foreground">{data?.title}</h1>
         </div>
     );
--- components/newsletter/Newsletter.tsx
+++ components/newsletter/Newsletter.tsx
@@ -1,53 +1,55 @@
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogTitle,
 } from "@radix-ui/react-dialog";
 import Image from "next/image";
 import exitButton from "../../public/exit.svg";
-import { Button } from "../ui/button";
 import { DialogHeader } from "../ui/dialog";
 import NewsletterComponent from "./NewsletterComponent";
 
-export const Newsletter = () => {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button>Åpne nyhetsbrev</Button>
-      </DialogTrigger>
-      <DialogContent className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50">
-        <DialogHeader>
-          <div
-            className={`relative bg-background-event max-w-xl border border-black flex flex-col pb-8 px-28 p-12 gap-5`}
-          >
-            <DialogClose asChild>
-              <button className="absolute top-0 right-0 h-8 w-8 mt-4 mr-4">
-                <Image
-                  src={exitButton}
-                  alt={"Exitknapp for å lukke nyhetsbrevpåmeldingen"}
-                />
-              </button>
-            </DialogClose>
-            <DialogTitle className="text-lg text-center font-normal">
-              Meld deg på nyhetsbrev fra ibruddet og få ekslusiv info, billetter
-              til redusert pris og andre tilbud!
-            </DialogTitle>
+interface NewsLetterProps {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}
 
-            <div className="w-100% self-center">
-              <NewsletterComponent />
-            </div>
+export const Newsletter = ({ open, setOpen }: NewsLetterProps) => {
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50">
+                <DialogHeader>
+                    <div
+                        className={`relative bg-background-event max-w-xl border border-black flex flex-col pb-8 px-28 p-12 gap-5`}
+                    >
+                        <DialogClose asChild>
+                            <button className="absolute top-0 right-0 h-8 w-8 mt-4 mr-4">
+                                <Image
+                                    src={exitButton}
+                                    alt={
+                                        "Exitknapp for å lukke nyhetsbrevpåmeldingen"
+                                    }
+                                />
+                            </button>
+                        </DialogClose>
+                        <DialogTitle className="text-lg text-center font-normal">
+                            Meld deg på nyhetsbrev fra ibruddet og få ekslusiv
+                            info, billetter til redusert pris og andre tilbud!
+                        </DialogTitle>
 
-            <script
-              src="https://cdn.dialogapi.no/form.min.js"
-              crossOrigin="anonymous"
-              async
-              defer
-            ></script>
-          </div>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
+                        <div className="w-100% self-center">
+                            <NewsletterComponent />
+                        </div>
+
+                        <script
+                            src="https://cdn.dialogapi.no/form.min.js"
+                            crossOrigin="anonymous"
+                            async
+                            defer
+                        ></script>
+                    </div>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
 };

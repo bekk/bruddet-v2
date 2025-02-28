3e5c0dd2195c4c468dafaa0f0df7653504776b53
Ole Norstrand <ole.norstrand@bekk.no>
2025-02-28 12:55:24 +0100

    fix(console-log): removed console.log

--- app/[locale]/(app)/layout.tsx
+++ app/[locale]/(app)/layout.tsx
@@ -7,7 +7,6 @@ type LayoutProps = {
 
 export default async function Layout({ children, params }: LayoutProps) {
     const lang = (await params).locale || "nb";
-    console.log("THIS IS LANG", lang);
     return (
         <div className="bg-background text-foreground min-h-front-page-height-mobile md:min-h-front-page-height h-full mb-footer-height-mobile md:mb-footer-height">
             {children}

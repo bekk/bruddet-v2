de84dcf638624e272908c296b829504ba1de3331
Anna Li <anna.li@bekk.no>
2025-03-06 14:36:26 +0100

    remove direct h1 styling

--- app/[locale]/(app)/meny/[slug]/ArticlePage.tsx
+++ app/[locale]/(app)/meny/[slug]/ArticlePage.tsx
@@ -21,7 +21,7 @@ export const ArticlePage = async ({ data, language }: ArticlePageProps) => {
   return (
     <div className="flex flex-col items-center">
       <div className="article-header flex flex-col max-w-7xl mx-auto gap-8 mt-24 mb-8 md:mb-24 px-6 md:px-12">
-        <h1 className="text-3xl lg:text-7xl font-normal text-center">{title}</h1>
+        <h1 className="font-normal text-center">{title}</h1>
         <h2 className="text-xl lg:text-3xl text-left lg:text-center font-normal lg:leading-[48px] break-words">
           {ingress}
         </h2>

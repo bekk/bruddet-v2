55dc7efea6e694dc63d968b43890b2f311020dc9
Anna Li <anna.li@bekk.no>
2025-03-06 14:37:39 +0100

    change from h2 to p

--- app/[locale]/(app)/meny/[slug]/ArticlePage.tsx
+++ app/[locale]/(app)/meny/[slug]/ArticlePage.tsx
@@ -22,9 +22,9 @@ export const ArticlePage = async ({ data, language }: ArticlePageProps) => {
     <div className="flex flex-col items-center">
       <div className="article-header flex flex-col max-w-7xl mx-auto gap-8 mt-24 mb-8 md:mb-24 px-6 md:px-12">
         <h1 className="font-normal text-center">{title}</h1>
-        <h2 className="text-xl lg:text-3xl text-left lg:text-center font-normal lg:leading-[48px] break-words">
+        <p className="text-xl lg:text-3xl text-left lg:text-center font-normal break-words">
           {ingress}
-        </h2>
+        </p>
 
         <TagButtons tagTexts={tagTexts} />
         {event && (

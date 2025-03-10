37352b8b8e95f67603f8812465185958b6fc4f2d
Ole Norstrand <ole.norstrand@bekk.no>
2025-03-10 09:12:40 +0100

    bug(scroller): Fixed the infinite scroll

--- components/footer/ScrollingCTA.tsx
+++ components/footer/ScrollingCTA.tsx
@@ -4,21 +4,21 @@ type ScrollingCTAProps = {
 
 export default function ScrollingCTA({ text }: ScrollingCTAProps) {
   return (
-    <div className="font-bold">
+    <>
       <div className="animate-marquee whitespace-nowrap">
         {Array.from({ length: 12 }).map((_, i) => (
-          <span key={i} className="m-4">
+          <span key={i} className="m-4 font-bold">
             {text}
           </span>
         ))}
       </div>
       <div className="absolute flex justify-center items-center animate-marquee2 whitespace-nowrap">
         {Array.from({ length: 12 }).map((_, i) => (
-          <span key={i} className="m-4">
+          <span key={i} className="m-4 font-bold">
             {text}
           </span>
         ))}
       </div>
-    </div>
+    </>
   );
 }

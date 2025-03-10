2c4440db36ff797d0693ed8bc3441c5bcb730587
Ole Norstrand <ole.norstrand@bekk.no>
2025-03-10 10:16:41 +0100

    refactor(ui): Update styling for buttons, badges, and links to improve consistency

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center uppercase rounded-md border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
@@ -13,12 +13,12 @@ const badgeVariants = cva(
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        outline: 'text-foreground uppercase text-sm font-bold',
+        outline: 'text-foreground',
       },
       size: {
-        default: 'min-h-10 px-4 py-2',
-        sm: 'min-h-9 rounded-md px-3',
-        lg: 'min-h-11 rounded-md px-4',
+        default: 'min-h-10 px-4 py-2 text-md',
+        sm: 'min-h-9 rounded-md px-3 text-sm',
+        lg: 'min-h-11 rounded-md px-4 text-lg',
         icon: 'h-10 w-10',
       },
     },
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,12 +5,12 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'relative border-black border-2 inline-flex items-center border justify-center gap-2 whitespace-nowrap rounded-md text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'uppercase relative border-black border-2 inline-flex items-center border justify-center gap-2 whitespace-nowrap rounded-md text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
         default:
-          'uppercase text-sm font-bold bg-primary text-primary-foreground hover:bg-primary-hover hover:underline hover:text-primary-foreground-hover before:absolute before:inset-0 before:border before:border-transparent before:hover:border-black before:pointer-events-none active:bg-primary-active active:underline active:text-primary-foreground',
+          'bg-primary text-primary-foreground hover:bg-primary-hover hover:underline hover:text-primary-foreground-hover before:absolute before:inset-0 before:border before:border-transparent before:hover:border-black before:pointer-events-none active:bg-primary-active active:underline active:text-primary-foreground',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
@@ -19,9 +19,9 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'min-h-10 px-4 py-2',
-        sm: 'min-h-9 rounded-md px-3',
-        lg: 'min-h-16 rounded-md px-8',
+        default: 'min-h-10 px-4 py-2 text-md',
+        sm: 'min-h-9 rounded-md px-3 text-sm',
+        lg: 'min-h-16 rounded-md px-8 text-lg',
         xl: 'min-h-14 rounded-md px-14 text-xl',
         icon: 'h-10 w-10',
       },

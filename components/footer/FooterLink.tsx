1c571628f7c68a23eea53b877021b693a0b89ab5
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-03-10 09:21:51 +0100

    Underline på siden man er inne på

--- components/footer/FooterLink.tsx
+++ components/footer/FooterLink.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useLocale, useTranslations } from 'next-intl';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function FooterLink({
   translationKey,
@@ -13,12 +14,14 @@ export default function FooterLink({
   link: string;
   className?: string;
 }) {
+  const pathname = usePathname();
   const locale = useLocale();
   const t = useTranslations('footer');
+  const isActive = pathname === `/${link}`;
   return (
     <Link
       href={`/${locale}/${link}`}
-      className={`md:border-0 border-foreground w-[50%] md:w-[15%] flex items-center sm:justify-center px-6 text-xl font-bold hover:bg-primary hover:text-primary-foreground hover:underline ${className}`}
+      className={`md:border-0 border-foreground ${isActive ? 'underline' : ''} w-[50%] md:w-[15%] flex items-center sm:justify-center px-6 text-xl font-bold hover:bg-primary hover:text-primary-foreground hover:underline ${className}`}
       aria-label={t(allyTranslationKey)}
     >
       {t(translationKey)}

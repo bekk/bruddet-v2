03c2f866783d62de4cbf62ee00ec2554da67118d
Ole Norstrand <ole.norstrand@bekk.no>
2025-03-10 10:20:34 +0100

    refactor(badge): Remove unused variants and update default variant to outline

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -8,22 +8,25 @@ const badgeVariants = cva(
   {
     variants: {
       variant: {
-        default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        secondary:
-          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        destructive:
-          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        // NBNB: DEFAULT, SECONDARY AND DESTRUCTIVE ARE NOT USED AND THUS NOT STYLED ACCORDINGLY
+        // default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
+        // secondary:
+        //   'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        // destructive:
+        //   'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
       },
       size: {
         default: 'min-h-10 px-4 py-2 text-md',
-        sm: 'min-h-9 rounded-md px-3 text-sm',
         lg: 'min-h-11 rounded-md px-4 text-lg',
-        icon: 'h-10 w-10',
+        // NBNB: SM AND ICON ARE NOT USED AND THUS NOT STYLED ACCORDINGLY
+        // sm: 'min-h-9 rounded-md px-3 text-sm',
+        // icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
-      variant: 'default',
+      // CHANGE VARIANT TO DEFAULT IF THAT IS STYLED AT SOME POINT
+      variant: 'outline',
       size: 'default',
     },
   },

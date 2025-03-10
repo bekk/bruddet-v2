1392ee6461ef866eeaf7c5223e3a69b79480a691
Anna Li <anna.li@bekk.no>
2025-03-10 12:46:30 +0100

    remove vertical scroll

--- components/footer/ScrollingCTA.tsx
+++ components/footer/ScrollingCTA.tsx
@@ -7,14 +7,14 @@ export default function ScrollingCTA({ text }: ScrollingCTAProps) {
     <>
       <div className="animate-marquee whitespace-nowrap">
         {Array.from({ length: 12 }).map((_, i) => (
-          <span key={i} className="m-4 font-bold">
+          <span key={i} className="mx-4 font-bold">
             {text}
           </span>
         ))}
       </div>
       <div className="absolute flex justify-center items-center animate-marquee2 whitespace-nowrap">
         {Array.from({ length: 12 }).map((_, i) => (
-          <span key={i} className="m-4 font-bold">
+          <span key={i} className="mx-4 font-bold">
             {text}
           </span>
         ))}

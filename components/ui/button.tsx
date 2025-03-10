ba05d69bf9fc9de1185b43f2e846ffddd4d11bae
Ole Norstrand <ole.norstrand@bekk.no>
2025-03-10 10:25:54 +0100

    refactor(button): Remove unused variants and clean up button styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,19 +11,23 @@ const buttonVariants = cva(
       variant: {
         default:
           'bg-primary text-primary-foreground hover:bg-primary-hover hover:underline hover:text-primary-foreground-hover before:absolute before:inset-0 before:border before:border-transparent before:hover:border-black before:pointer-events-none active:bg-primary-active active:underline active:text-primary-foreground',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:underline hover:bg-secondary-hover before:absolute before:inset-0 before:border before:border-transparent before:hover:border-black before:pointer-events-none active:bg-secondary-active',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+
+        // NBNB: THESE ARE NOT USED AND THUS NOT STYLED ACCORDINGLY
+        // outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        // ghost: 'hover:bg-accent hover:text-accent-foreground',
+        // link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'min-h-10 px-4 py-2 text-md',
-        sm: 'min-h-9 rounded-md px-3 text-sm',
         lg: 'min-h-16 rounded-md px-8 text-lg',
         xl: 'min-h-14 rounded-md px-14 text-xl',
-        icon: 'h-10 w-10',
+
+        // NBNB: THESE ARE NOT USED AND THUS NOT STYLED ACCORDINGLY
+        // sm: 'min-h-9 rounded-md px-3 text-sm',
+        // icon: 'h-10 w-10',
       },
     },
     defaultVariants: {

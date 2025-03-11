0e3fb36ad04a95d4ca808829d8b4e3e0171c9ffc
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-03-11 12:22:53 +0100

    add metadata to programpage

--- app/[locale]/(app)/program/page.tsx
+++ app/[locale]/(app)/program/page.tsx
@@ -1,6 +1,7 @@
 import { client } from '@/sanity/lib/client';
 import { PROGRAMPAGE_QUERY } from '@/sanity/lib/queries/programPage';
 import { PROGRAMPAGE_QUERYResult } from '@/sanity/types/types';
+import { Metadata } from 'next';
 import { ProgramPage } from './ProgramPage';
 
 export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
@@ -10,4 +11,24 @@ export default async function Page({ params }: { params: Promise<{ locale: strin
   });
 
   return <ProgramPage data={data} />;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const lang = (await params).locale;
+  const data: PROGRAMPAGE_QUERYResult = await client.fetch(PROGRAMPAGE_QUERY, {
+    lang,
+  });
+
+  return {
+    title: data?.metaTitle,
+    description: data?.metaDescription,
+    openGraph: {
+      title: data?.metaTitle as string,
+      description: data?.metaDescription as string,
+    },
+  };
 }

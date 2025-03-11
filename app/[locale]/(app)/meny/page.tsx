2a378309f8566a1f5b914370c2faaf9d4056442b
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-03-11 12:12:22 +0100

    Add metadata to menypage

--- app/[locale]/(app)/meny/page.tsx
+++ app/[locale]/(app)/meny/page.tsx
@@ -1,7 +1,8 @@
-import { MenuPage } from './MenuPage';
 import { client } from '@/sanity/lib/client';
 import { MENUPAGE_QUERY } from '@/sanity/lib/queries/menuPage';
 import { MENUPAGE_QUERYResult } from '@/sanity/types/types';
+import { Metadata } from 'next';
+import { MenuPage } from './MenuPage';
 
 export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
   const lang = (await params).locale;
@@ -10,4 +11,24 @@ export default async function Page({ params }: { params: Promise<{ locale: strin
   });
 
   return <MenuPage data={data} />;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const lang = (await params).locale;
+  const data: MENUPAGE_QUERYResult = await client.fetch(MENUPAGE_QUERY, {
+    lang,
+  });
+
+  return {
+    title: data?.metaTitle,
+    description: data?.metaDescription,
+    openGraph: {
+      title: data?.metaTitle as string,
+      description: data?.metaDescription as string,
+    },
+  };
 }

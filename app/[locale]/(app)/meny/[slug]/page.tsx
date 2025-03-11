e89dee391fbf6827dc67ae96593aafddb9d20bea
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-03-11 12:30:40 +0100

    Add metadata to ArticlePage

--- app/[locale]/(app)/meny/[slug]/page.tsx
+++ app/[locale]/(app)/meny/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { client } from '@/sanity/lib/client';
 
 import { ARTICLEPAGE_QUERY } from '@/sanity/lib/queries/articlePage';
 import { ARTICLEPAGE_QUERYResult } from '@/sanity/types/types';
+import { Metadata } from 'next';
 import { ArticlePage } from './ArticlePage';
 
 export default async function Page({
@@ -17,4 +18,24 @@ export default async function Page({
   });
 
   return <ArticlePage data={data} language={lang} />;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const lang = (await params).locale;
+  const data: ARTICLEPAGE_QUERYResult = await client.fetch(ARTICLEPAGE_QUERY, {
+    lang,
+  });
+
+  return {
+    title: data?.metaTitle,
+    description: data?.metaDescription,
+    openGraph: {
+      title: data?.metaTitle as string,
+      description: data?.metaDescription as string,
+    },
+  };
 }

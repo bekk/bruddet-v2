013a66328e36c3abdc4c55e624dd95fc7ceb6164
Ole Norstrand <ole.norstrand@bekk.no>
2025-03-12 09:00:18 +0100

    fix(typo): generage to generate

--- components/MainBlock.tsx
+++ components/MainBlock.tsx
@@ -7,10 +7,13 @@ import {
 
 type LeftBlockProps = {
   text: NonNullable<EVENT_QUERYResult>['text'] | NonNullable<ARTICLEPAGE_QUERYResult>['text'];
-  shouldGenerageH2Links?: boolean;
+  shouldGenerateH2Links?: boolean;
 };
 
-export const MainBlock = ({ text, shouldGenerageH2Links = false }: LeftBlockProps) => {
+export const MainBlock = ({
+  text,
+  shouldGenerateH2Links: shouldGenerateH2Links = false,
+}: LeftBlockProps) => {
   const leftBlockTypes = ['block', 'review', 'video', 'expandableBlock', 'faq'];
 
   return (
@@ -22,7 +25,7 @@ export const MainBlock = ({ text, shouldGenerageH2Links = false }: LeftBlockProp
             <div key={block._key} className={hidden}>
               <PortableText
                 components={
-                  shouldGenerageH2Links ? portableTextComponentsWithH2Tag : portableTextComponents
+                  shouldGenerateH2Links ? portableTextComponentsWithH2Tag : portableTextComponents
                 }
                 value={block}
               />

921d8a52454ceef412caa57a33fde216bac4ba01
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-03-18 12:33:26 +0100

    Always show b logo and always be red b logo

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,30 +1,18 @@
 'use client';
 import { useLocale, useTranslations } from 'next-intl';
-import RedLogo from './logo_red.svg';
-import BlackLogo from './logo_black.svg';
-import Link from 'next/link';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import RedLogo from './logo_red.svg';
 
 export default function Header() {
-  const frontpageUrl = ['/', '/en'];
-  const pathname = usePathname();
   const locale = useLocale();
   const t = useTranslations('header');
-
-  const isArticlePage = pathname.includes('/meny/');
-
-  const className = !frontpageUrl.includes(pathname) ? 'visible' : 'invisible';
 
   return (
     <>
       <div className="z-50 fixed flex items-center h-14 left-2 top-2">
-        <Link
-          className={className}
-          href={locale === 'nb' ? '/' : '/en'}
-          aria-label={t('go-to-main')}
-        >
-          <Image src={isArticlePage ? BlackLogo : RedLogo} alt="Logo" />
+        <Link href={locale === 'nb' ? '/' : '/en'} aria-label={t('go-to-main')}>
+          <Image src={RedLogo} alt="Logo" />
         </Link>
       </div>
     </>
